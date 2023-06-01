f9158abc3f8a2d27d2780f69b64aa231afab58b5
ltsme <4065818+ltsme@users.noreply.github.com>
2023-06-01 17:34:10 +0900

    0601
    사운드 추가
    아이콘 추가

--- OddEvenGame2/OddEvenGame2/ViewController.swift
+++ OddEvenGame2/OddEvenGame2/ViewController.swift
@@ -13,12 +13,17 @@
 
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet var comBallCountLbl: UILabel!
     @IBOutlet var userBallCountLbl: UILabel!
     @IBOutlet var resultLabel: UILabel!
+    @IBOutlet var imageUiView: UIView!
+    @IBOutlet var firstImage: UIImageView!
+    
+    var player: AVAudioPlayer?
     var comBallCount : Int = 20
     var userBallCount : Int = 20
     
@@ -28,29 +33,74 @@ class ViewController: UIViewController {
         
         userBallCountLbl.text = "user \(userBallCount) 남았습니다."
         comBallCountLbl.text = "com \(comBallCount) 남았습니다."
-    }
-
-
-    @IBAction func gameStart(_ sender: Any) {
+        self.imageUiView.isHidden = true
         
-        // 게임 시작시 나올 화면 알림, 버튼
-        let alert = UIAlertController(title: "GAME START", message: "홀 or 짝을 선택해 주세요", preferredStyle: .alert)
-        let oddBtn = UIAlertAction(title: "홀", style: .default){ _ in
-
+        self.play(fileName: "intro")
+    }
+    
+    func play(fileName : String) {
+        let filePath = Bundle.main.url(forResource: fileName, withExtension: "mp3")
+        
+        // filePath = URL? optional URL? 형태이므로 guard문을 통해 filePath가 nil일 경우 return
+        guard let path = filePath else {
+            return
+        }
+        
+        // 경로에 파일이 있을지 여부에 대한 try catch 예외처리
+        //self.player = try? AVAudioPlayer(contentsOf: filePath) // 결과 값이 없을 경우 nil을 반환
+        
+        // 경로에 파일이 있을지 여부에 대한 do catch
+        do {
+            self.player = try AVAudioPlayer(contentsOf: path)
             
-            guard let input = alert.textFields?.first?.text else {
+            guard let soundPlayer = self.player else {
                 return
             }
             
+            soundPlayer.prepareToPlay()
+            soundPlayer.play()
+            
+        }catch let error{
+            print("error : \(error.localizedDescription)")
+        }
+    }
+
+    @IBAction func gameStart(_ sender: Any) {
+        self.play(fileName: "gamestart")
+        self.imageUiView.isHidden = false
+        UIView.animate(withDuration: 2.0){
+            self.firstImage.transform = CGAffineTransform(scaleX: 5, y: 5  )
+            self.firstImage.transform = CGAffineTransform(scaleX: 1, y: 1)
+        }completion: { _ in
+            self.imageUiView.isHidden = true
+            self.showAlert()
+        }
+        
+    }
+    
+    func checkAccountEmpty(ball:Int) -> Bool {
+        return ball == 0
+    }
+    
+    func showAlert(){
+        // 게임 시작시 나올 화면 알림, 버튼
+        let alert = UIAlertController(title: "GAME START", message: "홀 or 짝을 선택해 주세요", preferredStyle: .alert)
+        let oddBtn = UIAlertAction(title: "홀", style: .default){ _ in
+            self.play(fileName: "click")
+
+            guard let input = alert.textFields?.first?.text else {
+                return
+            }
 
             guard let value = Int(input) else {
                 return
             }
             
             self.getWinner(count: value, select: "홀")
-            self
+            
         }
         let evenBtn = UIAlertAction(title: "짝", style: .default){ _ in
+            self.play(fileName: "click")
        
             guard let input = alert.textFields?.first?.text else {
                 return
@@ -74,6 +124,7 @@ class ViewController: UIViewController {
         // UIAlertController를 화면(ViewController)에 추가
         self.present(alert, animated: true)
     }
+    
     func getWinner(count : Int, select : String){
         let comNum = self.getRandom()
         let conNumType = comNum % 2 == 0 ? "짝" : "홀"
@@ -81,23 +132,32 @@ class ViewController: UIViewController {
         var result : String
      
         if(conNumType == select){
-            result =  "승리!"
+            result =  " User 승리!"
+            self.resultLabel.text = result
             self.calculate(winner: "user", count: count)
+            
         }else{
-            result =  "패배!"
+            result =  "User 패배!"
+            self.resultLabel.text = result
             self.calculate(winner: "com", count: count)
         }
-        resultLabel.text = result
+        
     }
     
     func calculate(winner : String, count : Int){
         if (winner == "com"){
             self.comBallCount = self.comBallCount + count
             self.userBallCount = self.userBallCount - count
+            if self.checkAccountEmpty(ball: self.userBallCount){
+                self.resultLabel.text = "컴퓨터의 최종 승리"
+            }
         } else if(winner == "user"){
             self.comBallCount = self.comBallCount - count
             self.userBallCount = self.userBallCount + count
-        }else{}
+            if self.checkAccountEmpty(ball: self.comBallCount){
+                self.resultLabel.text = "사용자의 최종 승리"
+            }
+        }
         self.userBallCountLbl.text = "user \(self.userBallCount) 남았습니다."
         self.comBallCountLbl.text = "com \(self.comBallCount) 남았습니다."
     }
